13991f5f41c3e208bcd61a8c2f83d58e53089403
fedi <fedi.dayeg@hotmail.com>
2021-11-17 20:40:51 +0100

    add the user and reports crud service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ItemsController } from './items/items.controller';
-
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './users/user.entity';
+import { Report } from './reports/report.entity';
 @Module({
-  imports: [],
-  controllers: [AppController, ItemsController],
+  controllers: [AppController],
   providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: '127.0.0.1',
+      database: 'db',
+      port: 5432,
+      username: 'postgres',
+      password: 'root',
+      synchronize: true,
+      entities: [User, Report],
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
 })
 export class AppModule {}
